b78803737be96240cc19324c542e1b96220e8e84
rasull7452 <rasullhossein250@gmail.com>
2025-10-23 16:06:22 +0200

    author

--- src/books/Author.java
+++ src/books/Author.java
@@ -1,4 +1,24 @@
 package books;
+import java.util.ArrayList;
+
 
 public class Author {
+    private String name;
+    private ArrayList<Title> titles;
+
+    public Author(String name){
+        this.name = name;
+    }
+
+    void addTitle(Title title){
+        titles.add(title);
+    }
+
+    double calculateRoyalties(){
+
+    }
+
+    public String getName() {
+        return name;
+    }
 }
